32be2f465ebbd0bf5ac4dfc4c2cd2653ea0c40d7
kyungjungkim <kyungjung.kim@gmail.com>
2025-03-13 16:35:20 +0900

    2025. 3. 13   SessionPreset 변경.

--- macOSCam/macOSCam/ContentViewController.swift
+++ macOSCam/macOSCam/ContentViewController.swift
@@ -44,7 +44,6 @@ class ContentViewController: NSViewController {
     
     private func setUpCamera() {
 //        captureSession.sessionPreset = .low
-//        captureSession.sessionPreset = .hd4K3840x2160
         selectSessionPreset()
         
         if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
@@ -66,40 +65,25 @@ class ContentViewController: NSViewController {
     }
     
     private func selectSessionPreset() {
-        /*if captureSession.canSetSessionPreset(AVCaptureSession.Preset.hd4K3840x2160) {
-            captureSession.sessionPreset = AVCaptureSession.Preset.hd4K3840x2160;
-            
-            //        A preset suitable for capturing 2160p-quality (3840 x 2160 pixels) video output.
-            print("B Camera - captureSessionPreset: \(captureSession.sessionPreset)")
-        } else if captureSession.canSetSessionPreset(AVCaptureSession.Preset.hd1920x1080) {
-            captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
-            
-            //        A preset suitable for capturing 1080p-quality (1920 x 1080 pixels) video output.
-            print("B Camera - captureSessionPreset: \(captureSession.sessionPreset)")
-        } else if captureSession.canSetSessionPreset(AVCaptureSession.Preset.hd1280x720) {
-            captureSession.sessionPreset = AVCaptureSession.Preset.hd1280x720
-            
-            //        A preset sitable for capturing 720p quality (1280 x 720 pixel) video output.
-            print("B Camera - captureSessionPreset: \(captureSession.sessionPreset)")
-        } else */ if captureSession.canSetSessionPreset(AVCaptureSession.Preset.iFrame1280x720) {
+        if captureSession.canSetSessionPreset(AVCaptureSession.Preset.iFrame1280x720) {
             captureSession.sessionPreset = AVCaptureSession.Preset.iFrame1280x720
             
-            //        A preset suitable for capturing 1280 x 720 quality iFrame H.264 video at about 40 Mbits/sec with AAC audio.
+            // A preset suitable for capturing 1280 x 720 quality iFrame H.264 video at about 40 Mbits/sec with AAC audio.
             print("B Camera - captureSessionPreset: \(captureSession.sessionPreset)")
         } else if captureSession.canSetSessionPreset(AVCaptureSession.Preset.iFrame960x540) {
             captureSession.sessionPreset = AVCaptureSession.Preset.iFrame960x540
             
-            //        A preset suitable for capturing 960 x 540 quality iFrame H.264 video at about 30 Mbits/sec with AAC audio.
+            // A preset suitable for capturing 960 x 540 quality iFrame H.264 video at about 30 Mbits/sec with AAC audio.
             print("B Camera - captureSessionPreset: \(captureSession.sessionPreset)")
         } else if captureSession.canSetSessionPreset(AVCaptureSession.Preset.cif352x288) {
             captureSession.sessionPreset = AVCaptureSession.Preset.cif352x288
             
-            //        A preset suitable for capturing CIF quality (352 x 288 pixel) video output.
+            // A preset suitable for capturing CIF quality (352 x 288 pixel) video output.
             print("B Camera - captureSessionPreset: \(captureSession.sessionPreset)")
         } else if captureSession.canSetSessionPreset(AVCaptureSession.Preset.photo) {
             captureSession.sessionPreset = AVCaptureSession.Preset.photo
             
-            //        A preset suitable for capturing high-resolution photo quality output.
+            // A preset suitable for capturing high-resolution photo quality output.
             print("B Camera - captureSessionPreset: \(captureSession.sessionPreset)")
         } else {
             print("B Camera - failed selectSessionPreset");
